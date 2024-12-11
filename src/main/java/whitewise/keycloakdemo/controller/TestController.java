de17f0a4303ecfd99862b226d1f0d430cce28b4c
whitewise95 <whitewise95@gmail.com>
2024-12-11 18:02:15 +0900

    필요없는 로직 제거 및 서버구성

--- src/main/java/whitewise/keycloakdemo/controller/TestController.java
+++ src/main/java/whitewise/keycloakdemo/controller/TestController.java
@@ -1,6 +1,12 @@
 package whitewise.keycloakdemo.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +20,40 @@ import whitewise.keycloakdemo.utils.JwtTokenUtils;
 
 @RestController
 @RequiredArgsConstructor
-public class HomeController {
+public class TestController {
 
 	@GetMapping("/code")
 	public ResponseEntity<String> handleAuthCode(@RequestParam("code") String code) {
 		return ResponseEntity.ok(code);
+	}
+
+	@GetMapping("/pkce")
+	public Map<String, String> pkce() throws NoSuchAlgorithmException {
+		String verifier = generateVerifier();
+		String CodeChallenge = generateCodeChallenge(verifier);
+
+		Map<String, String> map = new HashMap<>();
+		map.put("verifier", verifier);
+		map.put("codeChallenge", CodeChallenge);
+		return map;
+	}
+
+	private String generateVerifier() {
+		int length = 43 + new Random().nextInt(10); // Length: 43 to 53 (RFC 7636)
+		StringBuilder verifier = new StringBuilder();
+		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+		Random random = new Random();
+
+		for (int i = 0; i < length; i++) {
+			verifier.append(chars.charAt(random.nextInt(chars.length())));
+		}
+		return verifier.toString();
+	}
+
+	private String generateCodeChallenge(String verifier) throws NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] hash = digest.digest(verifier.getBytes(StandardCharsets.US_ASCII));
+		return Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
 	}
 
 	@PostMapping("/ciba-request")
