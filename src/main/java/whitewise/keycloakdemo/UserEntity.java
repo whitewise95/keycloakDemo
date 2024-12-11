8215d9e4c655b5225700b5a762ee51f4f6fdc2a6
whitewise95 <whitewise95@gmail.com>
2024-12-11 14:50:08 +0900

    lombok 적용

--- src/main/java/whitewise/keycloakdemo/UserEntity.java
+++ src/main/java/whitewise/keycloakdemo/UserEntity.java
@@ -9,6 +9,8 @@ import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
 @NamedQueries({
 	@NamedQuery(name = "getUserByUsername", query = "select u from UserEntity u where u.username = :username"),
@@ -20,6 +22,8 @@ import java.time.LocalDateTime;
 		"( lower(u.username) like :search or u.email like :search ) order by u.username"),
 })
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class UserEntity {
 
@@ -43,72 +47,8 @@ public class UserEntity {
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Boolean getEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
 	public String getFirstName() {
-		return this.name.substring(1,this.name.length());
+		return this.name.substring(1);
 	}
 
 	public String getLastName() {
